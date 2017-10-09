caea9d3b40a72dda1dfc77d410613968e6c405f0
Felipe Jaramillo <felipe.jaramillo@yuxiglobal.com>
2017-10-09 06:56:45 -0500

    add servicebus queue communication

--- IUGO.Shipping.Integration.TurnAssigned/Properties/AssemblyInfo.cs
+++ IUGO.Shipping.Integration.TurnAssigned/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IUGO.Turns.Integration.Handlers")]
+[assembly: AssemblyTitle("IUGO.Shipping.Integration.TurnAssigned")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IUGO.Turns.Integration.Handlers")]
+[assembly: AssemblyProduct("IUGO.Shipping.Integration.TurnAssigned")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("480336bc-4523-4928-9285-c6a7787f9ff3")]
+[assembly: Guid("58592c15-6055-40e8-8bd0-b36b90657398")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IUGO.Shipping.Integration.TurnAssigned/TurnAssigned.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using IUGO.Shipping.Integration.TurnAssigned;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using ServiceFabric.ServiceBus.Services;
+using ServiceFabric.ServiceBus.Services.CommunicationListeners;
+
+namespace IUGO.Shipping.Integration.TurnAssigned
+{
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class TurnAssigned : StatelessService
+    {
+        public TurnAssigned(StatelessServiceContext context)
+            : base(context)
+        {
+        }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            // In the configuration file, define connection strings: 
+            // "Microsoft.ServiceBus.ConnectionString.Receive"
+            // and "Microsoft.ServiceBus.ConnectionString.Send"
+
+            // Also, define a QueueName:
+            string serviceBusQueueName = "test"; //using entity path.
+            //alternative: CloudConfigurationManager.GetSetting("QueueName");
+            Action<string> logAction = log => ServiceEventSource.Current.ServiceMessage(base.Context, log);
+            yield return new ServiceInstanceListener(context => new ServiceBusQueueCommunicationListener(
+                new Handler(logAction)
+                , context
+                , serviceBusQueueName
+                , requireSessions: false)
+            {
+                AutoRenewTimeout =
+                    TimeSpan.FromSeconds(
+                        70), //auto renew up until 70s, so processing can take no longer than 60s (default lock duration).
+                LogAction = logAction,
+                MessagePrefetchCount = 10
+            }, "StatelessService-ServiceBusQueueListener");
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following sample code with your own logic 
+            //       or remove this RunAsync override if it's not needed in your service.
+
+            long iterations = 0;
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+
+    internal sealed class Handler : AutoCompleteServiceBusMessageReceiver
+    {
+
+        public Handler(Action<string> logAction)
+            : base(logAction)
+        {
+        }
+
+
+        protected override Task ReceiveMessageImplAsync(BrokeredMessage message, MessageSession session,
+            CancellationToken cancellationToken)
+        {
+            WriteLog(
+                $"Sleeping for 7s while processing queue message {message.MessageId} to test message lock renew function (send more than 9 messages!).");
+            Thread.Sleep(TimeSpan.FromSeconds(7));
+
+            WriteLog($"Handling queue message {message.MessageId}");
+            return Task.FromResult(true);
+        }
+
+    }
+}
